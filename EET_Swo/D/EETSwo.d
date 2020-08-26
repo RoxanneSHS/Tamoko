cc80f4d08ecd70bd9aeb0aaa76e13f7980f70489
RoxanneSHS <cwillenbockel@aol.com>
2020-08-26 17:34:32 +0200

    v 0.5

--- EET_Swo/D/EETSwo.d
+++ EET_Swo/D/EETSwo.d
@@ -1,83 +1,83 @@
 CHAIN
 IF WEIGHT #-2~Global("SWO_Delay","Global",2)~THEN Belt SwoQuest1
-~<CHARNAME>, your return indicates that you and your friends were successful.~
+@0
 DO~SetGlobal("SWO_Delay","Global",3)~
-==Liia IF~InParty("%IMOEN_DV%")~THEN~It is good to see you are well, Imoen. We have to talk, you and me - but first let us hear about Sarevok.~
+==Liia IF~InParty("%IMOEN_DV%")~THEN@1
 END
-++~Sarevok is dead. But on my return to here I was ambushed by remaining followers who are still loyal to his cause.~+ SwoQuest2
-++~Sarevok is history but he was not alone. Someone named Korlasz is still around with what remains of Sarevok's followers.~+ SwoQuest2
+++@2+ SwoQuest2
+++@3+ SwoQuest2
 
 CHAIN
 IF~~THEN Belt SwoQuest2
-~We have gained some information about Sarevok's background - and yours - while you were in the Undercity.~
-==Liia~Let us keep that Bhaalspawn issue aside for the moment, Belt. <CHARNAME> has proven <PRO_HISHER> alignment today.~
-==Belt~Very well, Liia Jannath. But it is important that the remaining followers of your so-called brother be eliminated and peace be restored in this town. This Korlasz you heard about is the heiress of a wealthy family here in town and probably one of Sarevok's sponsors.~
-==Liia~Our diviners say that her family possesses a large mausoleum right in the Undercity. She is down there, gathering the remaining forces of Sarevok's organisation.~
-==Belt~<CHARNAME>, need I remind you that there will be no peace for you until you put an end to them? You were and still are their target. It is not just our interest but your very own to search for them.~DO~EscapeAreaDestroy(45)~
-==Liia~You need to go once more to that Temple where your brother found his end.~DO~AddJournalEntry(@1004,QUEST)~
-==Liia IF~InParty("%IMOEN_DV%")~THEN~You follow your task, <CHARNAME>, but Imoen will stay with me for the time. Nothing to worry, my dear girl. It is a promise I gave to Gorion and Winthrop long ago.~
-==%IMOEN_JOINED% IF~InParty("%IMOEN_DV%")~THEN~You know Puffguts and ol' Gorion, lady?~
-==Liia IF~InParty("%IMOEN_DV%")~THEN~Yes, of course. I gave them a promise to take care of your further education when the time has come. Obviously that time is now. Come, my girl, it is the moment to think about yourself for a while, <CHARNAME> will be able to manage without you eventually. It will not be forever, be sure.~DO~EscapeAreaDestroy(45)~
-==%IMOEN_JOINED% IF~InParty("%IMOEN_DV%")~THEN~Ha, I doubt <PRO_HESHE> can, but hell...See you later, old buffleheads, I've got a career waitin'.~DO~LeaveParty() DestroySelf()~EXIT
+@4
+==Liia@5
+==Belt@6
+==Liia@7
+==Belt@8DO~EscapeAreaDestroy(45)~
+==Liia@9DO~AddJournalEntry(@1004,QUEST)~
+==Liia IF~InParty("%IMOEN_DV%")~THEN@10
+==%IMOEN_JOINED% IF~InParty("%IMOEN_DV%")~THEN@11
+==Liia IF~InParty("%IMOEN_DV%")~THEN@12DO~EscapeAreaDestroy(45)~
+==%IMOEN_JOINED% IF~InParty("%IMOEN_DV%")~THEN@13DO~LeaveParty() DestroySelf()~EXIT
 
 CHAIN
 IF WEIGHT #-4~AreaCheck("%NBaldursGate%") Global("TamokoJoin","GLOBAL",0)~THEN Tamoko AfterSar1
-~I see it already, you don't have to tell me...He did not listen, am I right, you had to do what had to be done, tell me.~
-=~Sarevok is dead?~
+@14
+=@15
 END
-++~Yes, he is, there was no way to reason with him.~+ AfterSar2
-++~I was not the person who could have stopped him. I was just another one who stood in his way. It was him or me.~+ AfterSar2
-++~Your heart told it to you but you didn't want to believe it, am I right? He would never have stood down.~+ AfterSar2
+++@16+ AfterSar2
+++@17+ AfterSar2
+++@18+ AfterSar2
 
 CHAIN
 IF~~THEN Tamoko AfterSar2
-~You are right, <CHARNAME>, I knew it would end this way. It was my foolish pride and blind love that made me hope for another ending.~
-=~I should have accepted the fact that I lost him already long time ago...~
-=~There is nothing for me to do any more for him. I need to forget him although I never will. But you need to go on and for you this journey has just begun. Will you do better with your heritage than he could? I would want to be witness and supporter for the one who withstands the blood. Let me follow you.~
+@19
+=@20
+=@21
 END
-++~Follow me like you followed him until you betrayed him. No, woman, get out of my sight while you still can.~DO~EscapeAreaDestroy(90)~EXIT
-++~What happened was not your fault, you tried to stay loyal as long as you could. You are someone I would like to get to know better. Join me.~DO~JoinParty()~EXIT
-++~First you betray Sarevok, now you try to poison me, snake. Are you in league with that Korlasz? I will not let you fool me, draw steel!~DO~Enemy()~EXIT
+++@22DO~EscapeAreaDestroy(90)~EXIT
+++@23DO~JoinParty()~EXIT
+++@24DO~Enemy()~EXIT
 
 CHAIN
 IF~Global("Swo_Ambush","%NBaldursGate%",1)~THEN BDKorme1 GetSword
-~There they are, this one there has the sword!~
+@25
 DO~SetGlobal("Swo_Ambush","%NBaldursGate%",2)~
-==BDKorme2~Risk nothing, they killed Sarevok, we only need the weapon for Korlasz.~
-==BDKorme1~Be quick before the guards get alarmed.~DO~ ClearAllActions() StartCutSceneMode() StartCutScene("SwoAmbS")~EXIT
+==BDKorme2@26
+==BDKorme1@27DO~ ClearAllActions() StartCutSceneMode() StartCutScene("SwoAmbS")~EXIT
 
 CHAIN
 IF WEIGHT #-5~Global("Safloot","%Undercity%",1)~THEN Safana ComeLoot
-~Darlings, you did it, really, you killed that bastard Sarevok. Too bad I missed the battle.~
+@28
 DO~SetGlobal("Safloot","%Undercity%",2)~
-=~Anyway, I'm just in time to help you carry all those valuables that must be stored inside. What you're waiting for?~
+=@29
 END
-++~I was only waiting for your encouragement, what else.~EXIT
-++~Forget it, greedy thief, what is inside there is mine, I earned it with my blood.~EXIT
-++~Scared, aren't you? Let me hold your hand, Safana, and fear nothing. I cleared the temple already for us.~EXIT
+++@30EXIT
+++@31EXIT
+++@32EXIT
 
 I_C_T BDKorlas 5 Swo_KorlTamok
-==TamokoJ IF~InParty("Tamoko")~THEN~Another fool who does not see when the dream is over. What do you think you still can gain?~
-==BDKorlas IF~InParty("Tamoko")~THEN~I am impressed by your undying love for Sarevok, traitress! He did right to kick you from his bed. So it was you who led them here so fast.~
-==TamokoJ IF~InParty("Tamoko")~THEN~It was not necessary to reveal anything to them, Korlasz, your plan was too clumsy and obvious. And don't call me a traitor when you sell Sarevok's sword the moment after he dropped it.~
-==BDKorlas IF~InParty("Tamoko")~THEN~It was...necessary. We needed the support to leave this town. I will not discuss this further, not with you, Tamoko, not with you!~
-==TamokoJ IF~InParty("Tamoko")~THEN~Who was the one to buy it?~
-==BDKorlas IF~InParty("Tamoko")~THEN~I wouldn't tell you even if I knew. The deal was through middlemen. Good gold and no questions, we were not in a position to reject it.~END
+==TamokoJ IF~InParty("Tamoko")~THEN@33
+==BDKorlas IF~InParty("Tamoko")~THEN@34
+==TamokoJ IF~InParty("Tamoko")~THEN@35
+==BDKorlas IF~InParty("Tamoko")~THEN@36
+==TamokoJ IF~InParty("Tamoko")~THEN@37
+==BDKorlas IF~InParty("Tamoko")~THEN@38END
 
  ADD_TRANS_TRIGGER Tamoko 23
 ~ReputationLT(Player1,9)~
 
 EXTEND_BOTTOM Tamoko 23
-IF~ReputationGT(Player1,8)~THEN REPLY~If I must kill you to pass, then so be it. I do so forgiving you your transgressions against me.~GOTO 21
-IF~ReputationGT(Player1,8)~THEN REPLY~If you would throw your life away, then you are already dead. If that is your choice, then you do not need me.~GOTO 25
+IF~ReputationGT(Player1,8)~THEN REPLY@39GOTO 21
+IF~ReputationGT(Player1,8)~THEN REPLY@40GOTO 25
 END 
 
 I_C_T BDIreni 23 Swo_IrenTamok
-==BDIreni IF~InParty("Tamoko")~THEN~(Points at Tamoko) Your Banite priestess could tell you of the power of dead gods, she followed one and then another who wanted to become heir. And now she follows the power she found in you.~
+==BDIreni IF~InParty("Tamoko")~THEN@41
 END
 
 I_C_T BDMadele 2 Swo_Madele
-==TamokoJ IF~InParty("Tamoko")~THEN~The faithful will be champions one day...~
-==BDMadele IF~InParty("Tamoko")~THEN~The Black Hand is just sleeping like my Lord sleeps. They will sweep the usurpers away when the day to return will come.~
-==TamokoJ IF~InParty("Tamoko")~THEN~They may or they may find their successors, who knows.~
-==BDMadele IF~InParty("Tamoko")~THEN~Bah, the Banites have reason to destroy Cyric as good as we have.~END
+==TamokoJ IF~InParty("Tamoko")~THEN@42
+==BDMadele IF~InParty("Tamoko")~THEN@43
+==TamokoJ IF~InParty("Tamoko")~THEN@44
+==BDMadele IF~InParty("Tamoko")~THEN@45END
--- EET_Swo/D/TamokoJ.d
+++ EET_Swo/D/TamokoJ.d
@@ -3,262 +3,262 @@ BEGIN TamokoP
 
 CHAIN
 IF~Global("TamokoJoin","GLOBAL",1)~THEN TamokoP GoOnce
-~I will never again degrade myself to begging a Bhaalspawn to keep me. We go our separate ways.~
+@0
 DO~SetGlobal("TamokoJoin","GLOBAL",0) EscapeAreaDestroy(60)~EXIT
 
 CHAIN
 IF~Global("Swo_TamokInt","Global",1)~THEN TamokoJ Underc1
-~Last time I was here I was ready to end my life. This time I am here to start a new one, <CHARNAME>.~
+@1
 DO~SetGlobal("Swo_TamokInt","Global",2)~
-=~Things can change rather quick, just like new plants springing out of the ashes of a bushfire.~
+=@2
 END
-++~Your moods swing rather wild, woman. From despair to rejoice within half a day.~+ Underc2
-++~You are over the death of the man you claimed to have loved very fast. Not long ago you were ready to give your life for him.~+ Underc2
-++~Such flowery words from the one who wanted to be the chosen of the new God of Murder? It's hard to believe you.~+ Underc2
+++@3+ Underc2
+++@4+ Underc2
+++@5+ Underc2
 
 CHAIN
 IF~~THEN TamokoJ Underc2
-~It took long, really much too long for me to awake. I was still clinging to Sarevok when everything was telling me already how useless my loyalty and persistence was. He was never to be what he wanted to be, he was just fuel for his father's return.~
-=~You...you have seen what will happen, didn't you? You grew up in Candlekeep with the chanters singing Alaundo's words all the time. Are you warned? Are you strong enough?~
+@6
+=@7
 END
-++~Stop it already. Come to your senses, Tamoko, you scare me.~+ Underc3
-++~You read all the prophesy with your former lover, didn't you?~+ Underc3
-++~Let us finish that final quest ahead and leave this darkness, Tamoko. It stresses your nerves like it does mine.~+ Underc3
+++@8+ Underc3
+++@9+ Underc3
+++@10+ Underc3
 
 CHAIN
 IF~~THEN TamokoJ Underc3
-~Bhaalspawns die to return their essence to its source, <CHARNAME>. Sarevok's death gives strength to the Throne of Murder. Your death would do the same. Sarevok thought that causing death would give HIM strength. Do not make that same mistake.~EXIT
+@11EXIT
 
 CHAIN
 IF~Global("Swo_TamokInt","Global",3)~THEN TamokoJ Korla1
-~This Korlasz had no interest in Sarevok's sword, she sold it to someone in Amn, <CHARNAME>.~
+@12
 DO~SetGlobal("Swo_TamokInt","Global",4)~
-=~Keeping a relict for the cult of the god-that-never-was would be one thing but there seems to be something else going on here.~
+=@13
 END
-++~Do you have some more insight into that Korlasz's plans, you must know her?~+ Korla2
-++~What do you suspect is going on, you were one of them until recently?~+ Korla2
+++@14+ Korla2
+++@15+ Korla2
 
 CHAIN
 IF~~THEN TamokoJ Korla2
-~That sword may be a relict representing Sarevok for some, but there may be another interest in the weapon...~
-=~(She points to a slash that started healing on your forearm. You received it in the battle with your *brother*). Blood, <CHARNAME>, your blood.~
+@16
+=@17
 END
-++~I see, my blood which is Bhaalblood.~+ Korla3
-++~What would anyone want with a weapon that has some dried blood on it.~+ Korla3
-++~Recent events have done your mind no good, Tamoko, your wild fantasies are better kept to yourself.~+ Korla3
+++@18+ Korla3
+++@19+ Korla3
+++@20+ Korla3
 
 CHAIN
 IF~~THEN TamokoJ Korla3
-~Korlasz is a rich heiress in Baldur's Gate who followed Sarevok partly out of boredom and maybe hoping to win my place one day. Sarevok cared not for her, only for her gold. She bothers me not. But you should be aware of the one from Amn who seems to track you.~
-=~I knew they would come and I often warned Sarevok. Those that are not interested in Bhaal but in the powers that a Bhaalspawn gains when growing and which they want to use for their purpose.~
+@21
+=@22
 END
-++~To be used as a weapon for someone's purpose? I have heard this theory somewhere already.~+ Korla4
-IF~InParty("CVSandr")~THEN REPLY~Now you start to sound just like Sandrah with her hints and warnings.~+ Korla5
-++~After I saved the Sword Coast from a war and from your lover it's no wonder that people get interested in me.~+ Korla4
+++@23+ Korla4
+IF~InParty("CVSandr")~THEN REPLY@24+ Korla5
+++@25+ Korla4
 
 CHAIN
 IF~~THEN TamokoJ Korla4
-~Stay alert, that is my advice. You want not become the pawn in someone's game. Winski tried it with Sarevok, he wanted to use him as a means to revenge on Rashemen, who expelled him. Winski was too weak but your pursuers may be stronger.~EXIT
+@26EXIT
 
 CHAIN
 IF~~THEN TamokoJ Korla5
-~She is often right with what she says although I would not trust her if I were you. She has more hidden intentions than anyone else around.~EXIT
+@27EXIT
 
 CHAIN
 IF~Global("Swo_Sareroom","BD0130",1) ~ THEN TamokoJ Korsec1
-~Ah, Sarevok's little secret! Three arrangements of coloured torches, one is missing the purple torch.~
+@28
 DO~SetGlobal("Swo_Sareroom","BD0130",2)~
 END
-++~There were some coloured braziers in the other room. I wonder what happens if we restore the missing torch with the right coloured one.~+ Korsec2
-++~Do you have any idea what type of secret your former lover hid down here?~+ Korsec2
+++@29+ Korsec2
+++@30+ Korsec2
 
 CHAIN
 IF~~THEN TamokoJ Korsec2
-~I have no idea which of his many secrets we might reveal but I am sure that the place is  not unguarded. Be ready to fight for the answer, <CHARNAME>.~EXIT
+@31EXIT
 
 CHAIN
 IF~Global("Swo_TamokInt","Global",5)~THEN TamokoJ Palace1
-~Ha! Life is full of antics, <CHARNAME>. I stand here in the Ducal Palace of Baldur's Gate side by side with a Bhaalspawn just like it was promised to me long ago. And everything went different from what was planned.~
+@32
 DO~SetGlobal("Swo_TamokInt","Global",6)~
 END
-++~The duchess Anchev? Was that your dream of life?~+ Palace2
-++~Ambitions made Sarevok fail, and your own ambitions have hardly done you any good.~+ Palace2
+++@33+ Palace2
+++@34+ Palace2
 
 CHAIN
 IF~~THEN TamokoJ Palace2
-~Without ambitions there wouldn't have been my failures, so much is true. But that makes not trying to achieve something not an option for me. Not trying would have meant a squalid life for me.~
-=~Except for my poor mother, nobody ever cared about a fatherless Kara-Tur halfbreed. From where I started there is only one way and that leads uphill. Everybody can fall down but those at the bottom will not stay down if they have a choice.~
-=~One may listen to the false people's promises but the urge to move is too strong to ignore whichever option appears before you.~
+@35
+=@36
+=@37
 END
-++~This drove you into the faith of an evil god, is that so?~+ Palace3
-++~Sarevok used you but you seemed to have used him just the same, it appears to me.~+ Palace3
-++~Some learn from mistakes and some justify them. Which type are you?~+ Palace3
+++@38+ Palace3
+++@39+ Palace3
+++@40+ Palace3
 
 CHAIN
 IF~~THEN TamokoJ Palace3
-~(Tamoko has already moved ahead, ignoring your last question.)~
+@41
 EXIT
 
 CHAIN
 IF~Global("Swo_SkieReact","Locals",2)~THEN TamokoJ Skie1
-~(Mumbles) Skie Silvershield, so near but yet unreachable...~
+@42
 DO~SetGlobal("Swo_SkieReact","Locals",3)~
 END
-++~Did you meet her before, Tamoko?~+ Skie2
-++~Unreachable? She is here in the camp.~+ Skie2
-++~What?~+ Skie2
+++@43+ Skie2
+++@44+ Skie2
+++@45+ Skie2
 
 CHAIN
 IF~~THEN TamokoJ Skie2
-~(Startled) Was I thinking aloud? I was contemplating Sarevok's list, I guess he never met her.~
+@46
 END
-++~A list? Now you have me confused. Why did Sarevok have a list with Skie's name on it.~+ Skie3
+++@47+ Skie3
 
 CHAIN
 IF~~THEN TamokoJ Skie3
-~I...well, now that I started, why not go on. I knew Skie was in Baldur's Gate all the time and thus very near to us, Sarevok and me. But he never met her.~
-=~You see, Sarevok had a list of people he intended to meet someday. Your name was on it and Skie's and many more. He scratched out the ones he was done with. Even with Skie being near all the time, her name was still unmarked when I last saw the paper. His family list.~
+@48
+=@49
 END
-++~My name? What does that mean. We met in Candlekeep where he disguised as Koveras.~+ Skie4
-++~Family list? Neither me nor Skie Silvershield were his relatives.~+ Skie4
+++@50+ Skie4
+++@51+ Skie4
 
 CHAIN
 IF~~THEN TamokoJ Skie4
-~*<CHARNAME>, Gorion's ward*, I remember the entry. But it was crossed out long before he went to Candlekeep to eliminate Rieltar and the others. ~
-=~Anyway, I was just lost in memories. It does not help us here today. Let us forget it and move on.~EXIT
+@52
+=@53EXIT
 
 CHAIN
 IF~Global("Swo_SkieReact","Locals",4) ~THEN TamokoJ Gorion1
-~Recently I remembered the very first time we met, <CHARNAME>. Do you recall it?~
+@54
 DO~SetGlobal("Swo_SkieReact","Locals",5) ~
 END
-++~You came to betray Sarevok, in the streets of Baldur's Gate we met.~+ Gorion1a
-++~I got a glance of you before Gorion ordered me to run to safety.~+ Gorion1b
-++~I don't remember it any more, to be honest.~+ Gorion1c
+++@55+ Gorion1a
+++@56+ Gorion1b
+++@57+ Gorion1c
 
 CHAIN
 IF~~THEN TamokoJ Gorion1a
-~I did never betray Sarevok, I tried to save his life, Anyway, that was not our first meeting.~EXTERN TamokoJ Gorion2
+@58EXTERN TamokoJ Gorion2
 
 CHAIN
 IF~~THEN TamokoJ Gorion1b
-~You did well to listen to him otherwise you wouldn't be here today.~EXTERN TamokoJ Gorion2
+@59EXTERN TamokoJ Gorion2
 
 CHAIN
 IF~~THEN TamokoJ Gorion1c
-~I must have impressed you as little as you impressed me on that occasion.~EXTERN TamokoJ Gorion2
+@60EXTERN TamokoJ Gorion2
 
 CHAIN
 IF~~THEN TamokoJ Gorion2
-~It was outside of Candlekeep on the day Gorion died. He gave his life for yours. You just ran. A pathetic weakling compared to Sarevok. You are here today while he and Gorion are both dead.~  
+@61  
 END
-++~You attacked Gorion with spells while Sarevok did the hackwork.~+ Gorion3
-++~Neither of you scared me for too long. It was an ambush and a surprise. You were lucky, Gorion sadly was not.~+ Gorion3
-++~Those two are dead - and many more. You and I are still alive.~+ Gorion3
+++@62+ Gorion3
+++@63+ Gorion3
+++@64+ Gorion3
 
 CHAIN
 IF~~THEN TamokoJ Gorion3
-~I did not bring this up to dwell on memories. I compare the <PRO_MANWOMAN> you are today with the fledgling you were then. You grew fast and you are growing still. It is impressive and it gives me confidence. Many have died since that day. You and I are still alive. We have something they had not. Make sure you do not lose it, <CHARNAME>.~EXIT
+@65EXIT
 
 CHAIN
 IF~Global("Swo_TamokInt","Global",7)~THEN TamokoJ Swordire1
-~Sarevok's missing sword of chaos...This Caelar does not have it. She did not need a sample of your blood to know about your heritage.~
+@66
 DO~SetGlobal("Swo_TamokInt","Global",8)~
 END
-++~Didn't you mention someone in Amn who would be interested?~+ Swordire2
-++~What does the sword mean to you, Tamoko, a symbol of your lost love?~+ Swordire3
-++~The sword, I almost forgot about it. Why would it be important.~+ Swordire2 
+++@67+ Swordire2
+++@68+ Swordire3
+++@69+ Swordire2 
 
 CHAIN
 IF~~THEN TamokoJ Swordire2
-~Korlasz or her servants mentioned it. The sword is nothing, the interest someone may have in your blood is disturbing. The sooner we finish this foolish Shining Lady, the sooner we can take care of that matter.~
+@70
 END 
-++~What does the sword mean to you, Tamoko, a symbol of your lost love?~+ Swordire3
-++~The Shining Lady is our opponent here and now. Your phantom sword thief is secondary only.~+ Swordire4
+++@68+ Swordire3
+++@71+ Swordire4
 
 CHAIN
 IF~~THEN TamokoJ Swordire3
-~Do you really think that I loved Sarevok just because I shared his bed?~
+@72
 END
-++~You were begging for his life when we first met.~+ Swordire4
-++~Are you saying that you lied about your love to him and to me. You just used him like all the others?~+ Swordire4
+++@73+ Swordire4
+++@74+ Swordire4
 
 CHAIN
 IF~~THEN TamokoJ Swordire4
-~You still understand very little of what is going on, <CHARNAME>. Sarevok was not the one to claim the throne of his father. All his death has resulted in, was to provide more bhaalspawn essence to the abyss. Nobody has an interest in that and I tried to prevent it. We do not want to wake up Bhaal. The time is not yet for anyone to try it.~ EXIT
+@75 EXIT
 
 CHAIN
 IF~ Global("Swo_Madele","Global",2)~THEN TamokoJ AfterMadel1
-~Nothing is permanent, <CHARNAME>, victories and defeats are temporary events in history's flow.~
+@76
 DO~SetGlobal("Swo_Madele","Global",3)~
 END
-++~Does that refer to Cyric's taking the portfolio of the Dead Three?~+ AfterMadel2
-++~I still wonder what you and Madele were talking about back in that temple.~+ AfterMadel2
-++~That temple visit seems to have left you a bit uneasy. Your past catches up with you.~+ AfterMadel2
+++@77+ AfterMadel2
+++@78+ AfterMadel2
+++@79+ AfterMadel2
 
 CHAIN
 IF~~THEN TamokoJ AfterMadel2
-~My past is the key to my future. The mad Cyric is just an upstart without substance. The heirs of Jergal will reclaim their power in just a while.~
+@80
 END
-++~The heirs of Jergal are the Dead Three, and they have that name, well, because they are dead.~+ AfterMadel3
-++~Dream on, just like Sarevok dreamt and that blind fool dreams. None of those dead gods is supposed to threaten us again.~+ AfterMadel3
-++~You carry a large load of death gods and mortals with you, Tamoko. Wake up and start to deal with those who live.~+ AfterMadel3
-IF~!InParty("CVSandr")~THEN REPLY~Whether is is Bhaal or it is Bane, you want to be on the winning side. Your ambitions will be your downfall, Tamoko, since none of the two will happen.~+ AfterMadel3
+++@81+ AfterMadel3
+++@82+ AfterMadel3
+++@83+ AfterMadel3
+IF~!InParty("CVSandr")~THEN REPLY@84+ AfterMadel3
 
 CHAIN
 IF~~THEN TamokoJ AfterMadel3
-~Those who think of themselves as living powers may not be it for long. Those assumed to be forever gone may return sooner than they can be forgotten. Make sure you take that into account when making choices, <CHARNAME>. Learn from what Sarevok refused to learn.~EXIT
+@85EXIT
 
 CHAIN 
 IF ~ Global("Swo_camp","LOCALS",1)~THEN TamokoJ Campfire1
-~(You wake up to find that it is still dark and your companions are asleep, except for...)~
+@86
 DO~SetGlobal("Swo_camp","LOCALS",2)~
-=~(Tamoko sits near the campfire staring into the dying flames. Given that she must be twice your age, or Sarevok's, she is still a very attractive woman with the exotic touch of her Kara-Turan heritage.)~
+=@87
 END
-++~Was it really love that connected you and Sarevok or was it rather some alliance to strive for power?~+ Campfire2
-++~You turn back to your pillow. You need all the rest you can get to face the challenge of the next day.)~DO~StartMovie("Rest")~EXIT
-++~Was it just Cythandria's youth that made Sarevok turn to her and abandon you?~+ Campfire2
+++@88+ Campfire2
+++@89DO~StartMovie("Rest")~EXIT
+++@90+ Campfire2
 
 CHAIN
 IF~~THEN TamokoJ Campfire2
-~You are a good observer, <CHARNAME>. I don't deny all of us were ambitious - and angry. We wanted to show all those in Baldur's Gate that treated us like outcasts our power, make them kneel before us and ask for mercy. We envisioned the scenes and our satisfaction from them before we went between the sheets. Our lust for power and our lust for each other were fierce and strong.~
+@91
 END
-++~Lust is not love, Tamoko.~+ Campfire3
-++~A relationship built on such foundation was supposed to fail when a younger woman emerges, like Cythandria.~+ Campfire3
+++@92+ Campfire3
+++@93+ Campfire3
 
 CHAIN
 IF~~THEN TamokoJ Campfire3
-~I have asked myself the same again and again. I thought I loved Sarevok. I wished for it to be true. And maybe it was for a time. But there were too many other aspects in our relationship and love was not the most important.~
-=~Sarevok changed and probably I changed as well. That mage whore was just a symptom of the disease that had grown in Sarevok's heart. He may have loved me once but he lost the ability to love when his ideas and visions began to eat him up. He surely never loved Cythandria.~
+@94
+=@95
 END
-++~And you, are you sure you stopped loving him?~+ Campfire4
-++~I don't think Sarevok was ever able to love anyone but himself. I'm not even sure you are.~+ Campfire4
+++@96+ Campfire4
+++@97+ Campfire4
 
 CHAIN
 IF~~THEN TamokoJ Campfire4
-~You are even younger than he was. You learned too little of life behind those walls of Candlekeep. Keep your illusions of love, forgiveness, dreams that come true...~
-=~We are out here fighting for survival, we all are. ~
-=~Stop talking - we need a good night's rest to be fit for what awaits us.~DO~StartMovie("Rest")~EXIT
+@98
+=@99
+=@100DO~StartMovie("Rest")~EXIT
 
 CHAIN
 IF~Global("Swo_Desease","Locals",2)~ THEN TamokoJ Diseased
-~Now I will never see Kara-Tur...I always hoped I would someday.~
+@101
 DO~SetGlobal("Swo_Desease","Locals",3)~
-=~You may ask why...I was not even born there. It has been a curse all my life. You suffer from your blood and I did from mine, the father I never saw who bequeathed me with a homeland I've never set foot on...(Cough).~
-=~Ha! Life is full of antics, <CHARNAME>.~ DO~CreateVisualEffectObject("SPHOLY",Myself) ActionOverride(Player1,DisplayStringHead(Myself,@1007)) DropInventory() Wait(2)LeaveParty()ApplySpellRES("SPWI055",Myself) DestroySelf()~EXIT
+=@102
+=@103 DO~CreateVisualEffectObject("SPHOLY",Myself) ActionOverride(Player1,DisplayStringHead(Myself,@1007)) DropInventory() Wait(2)LeaveParty()ApplySpellRES("SPWI055",Myself) DestroySelf()~EXIT
 
 CHAIN
 IF~Global("Swo_InJail","Global",2)~THEN TamokoJ Jaildead
-~Following Bhaalspawns was my fate and seals my end, <CHARNAME>.~
+@104
 DO~SetGlobal("Swo_InJail","Global",3)~
-=~The Flaming Fist does not forgive their own errors to trust first Sarevok and then you. I was on their warrant list already, your downfall allowed them to strike.~
+=@105
 END
-++~Tamoko, I'm sorry, but I can't help you at this moment. I'll come back if I can.~+ Jaildead2
-++~That was not foreseen. I wasn't aware they were still after you.~+ Jaildead2
-++~What will happen to you, Tamoko?~+ Jaildead2
+++@106+ Jaildead2
+++@107+ Jaildead2
+++@108+ Jaildead2
 
 CHAIN
 IF~~THEN TamokoJ Jaildead2
-~There is nothing that you or anyone else can do for me now. The Dukes and the Fist want to see me hang, just as a symbol and scapegoat for their own mistakes.~
-=~I won't give them that show. Talona spoils their plan. My end is my own and dedicated to my Lord - Bane.~EXTERN TamokoJ Diseased
+@109
+=@110EXTERN TamokoJ Diseased
  
